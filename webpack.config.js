7369a8734e3770b49dadd9afed13dfbf96680439
Jaga Apple <jagaapple@uniboar.com>
2019-05-06 05:54:20 +0900

    [Add] Add copyright comments to a built file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require("path");
+const webpack = require("webpack");
 
 const srcDir = path.resolve(__dirname, "src");
 const distDir = path.resolve(__dirname, ".dist");
+
+const version = require("./package.json").version;
+const copyright = `
+aws-actions-list v${version}
+https://github.com/jagaapple/aws-actions-list
+
+Copyright 2019, Jaga Apple and other contributors.
+Licensed under MIT (https://github.com/jagaapple/aws-actions-list/blob/master/LICENSE)
+
+Date: 2019-05-06T00:00Z
+`;
 
 module.exports = {
   mode: "production",
@@ -20,4 +32,5 @@ module.exports = {
       { test: /\.ts$/, loader: "ts-loader" },
     ],
   },
+  plugins: [new webpack.BannerPlugin(copyright)],
 };
