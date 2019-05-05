14ced18e90682fa68a62bd42d3cde4b04bcb1340
Jaga Apple <jagaapple@uniboar.com>
2019-05-06 01:22:24 +0900

    [Add] Implement interactive outputting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
 const { getActions } = require("./actions-getter");
 
+const supportedServiceNames = fs
+  .readdirSync(path.join(__dirname, "source-definitions"))
+  .map((filename) => filename.split(".")[0]);
+
 (async () => {
-  const sourceDefinition = require("./source-definitions/s3.json");
+  const { service } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "service",
+      message: "Select a service (the following is supported services)",
+      choices: supportedServiceNames,
+    },
+  ]);
+  const sourceDefinition = require(path.join(__dirname, "source-definitions", `${service}.json`));
   const actions = await getActions(sourceDefinition);
 
   console.log(actions);
